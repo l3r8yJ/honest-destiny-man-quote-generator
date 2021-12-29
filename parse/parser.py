7e602a20d0aa712e419a99afe713c0379172bc56
l3r8yJ <clicker.heroes.acg@gmail.com>
2021-12-29 15:52:46 +0300

    data update

--- parse/parser.py
+++ parse/parser.py
@@ -11,7 +11,7 @@ data = []
 wall = vk.wall.get(owner_id=-202146149, count=100)
 
 for i, post in enumerate(wall['items']):
-    if i > 2:
+    if i >= 2:
         if (len(post['text']) != 0):
             line = post['text'].replace('\n', '')
             if(line.find('"') != -1 and line.find('Праздник') == -1):
@@ -23,6 +23,6 @@ except FileNotFoundError:
     print('File not found')
 
 for line in data:
-    f.write(line + ';')
+    f.write(line + ';\n')
 
 f.close()
